4f663c39e516a41414bab87967f464975c6bb73d
IJuanI <juancojcl@hotmail.com>
2017-04-13 14:45:39 -0300

    Initial Commit

---
+++ Assets/Assets/Scripts/BackgroundSlider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundSlider : MonoBehaviour {
+
+	public float scrollSpeed = 1;
+	private Rect savedOffset;
+
+	void Start () {
+		savedOffset = GetComponent<RawImage>().uvRect;
+	}
+
+	void Update () {
+		float x = Mathf.Repeat (Time.time * 0.1f * scrollSpeed, 1);
+		Rect offset = savedOffset;
+		offset.x = x;
+		GetComponent<RawImage>().uvRect = offset;
+	}
+
+	void OnDisable () {
+		GetComponent<RawImage>().uvRect = savedOffset;
+	}
+}
---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float jumpHeight = 4;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space))
+			tryJump ();
+	}
+
+	void tryJump() {
+		GetComponent<Rigidbody2D> ().velocity = Vector3.up * jumpHeight;
+	}
+}
