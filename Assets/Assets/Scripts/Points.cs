3a95c9403fdf61ca9c4cf4c1bf1f0a1cef60fa52
Hisu <nachocuatrin@gmail.com>
2017-04-17 16:29:16 -0300

    Minor Fix: Point Reset

--- Assets/Assets/Scripts/Points.cs
+++ Assets/Assets/Scripts/Points.cs
@@ -6,16 +6,27 @@ using UnityEngine.UI;
 public class Points : MonoBehaviour {
 	private Text m_Text;
 	double aux;
+	int cont;
 	private void Start()
 	{
+		aux = 0;
 		m_Text = GetComponent<Text>();
+		cont = 0;
 	}
 
 	private void Update()
 	{
 		if (GameManager.loser==false)
 		{
-			aux = Math.Truncate (10 * Time.time);
+			if (cont == 10) 
+			{
+				aux = aux + 1;
+				cont=0;
+			} 
+			else 
+			{
+				cont++;
+			}
 			m_Text.text = "Puntaje: " + aux.ToString ();
 		}
 	}
