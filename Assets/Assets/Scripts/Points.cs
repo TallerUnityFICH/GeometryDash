6c98e8099be91dca2b57a1c958dad1c71f3c43e7
Hisu <nachocuatrin@gmail.com>
2017-04-17 16:16:10 -0300

    Add Points system

---
+++ Assets/Assets/Scripts/Points.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof (Text))]
+public class Points : MonoBehaviour {
+	private Text m_Text;
+	double aux;
+	private void Start()
+	{
+		m_Text = GetComponent<Text>();
+	}
+
+	private void Update()
+	{
+		if (GameManager.loser==false)
+		{
+			aux = Math.Truncate (10 * Time.time);
+			m_Text.text = "Puntaje: " + aux.ToString ();
+		}
+	}
+}
