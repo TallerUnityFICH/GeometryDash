6df34a7b6009f3e3a19136e7978c3fbc43ea80b8
Hisu <nachocuatrin@gmail.com>
2017-04-18 08:45:08 -0300

    Revert "Lifebar Implementation"
    This reverts commit 3cf9543b3df113b9f5a86a3b882ad0723b4e19a8.

--- Assets/Assets/Scripts/GameManager.cs
+++ Assets/Assets/Scripts/GameManager.cs
@@ -29,7 +29,7 @@ public class GameManager : MonoBehaviour {
 	Vector3 rotation = new Vector3 (0, 0, 2);
 	float animDelay;
 	int animIndex = 0;
-	public static int lifes = 1;
+	int lifes = 1;
 
 	void Start () {
 		global = this;
@@ -82,6 +82,9 @@ public class GameManager : MonoBehaviour {
 			lifes--;
 			return;
 		}
+
+		lifes = 2;
+
 		GameOver ();
 	}
 
@@ -103,7 +106,6 @@ public class GameManager : MonoBehaviour {
 		if (canRestart) {
 			global.controller.showMenu (false);
 			global.StartCoroutine ("delayReset");
-			lifes = 1;
 		}
 	}
 
